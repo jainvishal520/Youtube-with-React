d892a5274ab346e634cb392d5a77553a7412dc61
jainvishal12 <vjtinu.jain431@gmail.com>
2017-03-27 12:28:21 +0530

    Building lists with map

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyCVw6GOerZbEawmJZLkgEWT6j-rL2m3hdI'
 
-YTSearch({key : API_KEY , term : 'surfboards'},(data) => console.log(data))
+
 //Create a new component.This component should produce some html
 class App extends Component{
 	constructor(props){
@@ -13,12 +14,14 @@ class App extends Component{
 		this.state = {videos : []};
 		YTSearch({key : API_KEY,term : 'smushball'},(videos)=>{
 			this.setState({videos});
+			//this.setState({videos:videos})
 		});
 	}
 	render(){
 		return(
 			<div>
 				<SearchBar />
+				<VideoList videos={this.state.videos} />
 			</div>
 		)
 	}
@@ -26,4 +29,5 @@ class App extends Component{
 
 //Take this components generated html and 
 //put it on the page(in the DOM)
+
 ReactDOM.render(<App />,document.querySelector('.container'));
